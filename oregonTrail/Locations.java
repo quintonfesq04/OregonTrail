2bcb70a001f41e754e31ec8b36b1fc74b9e85658
l-kanagy <143094036+l-kanagy@users.noreply.github.com>
2024-04-15 22:48:08 -0400

    Locations.java
    Added more

--- oregonTrail/Locations.java
+++ oregonTrail/Locations.java
@@ -6,10 +6,44 @@
  *location, and outputs the distance left to travel. It is used as the go-to class for
  *and calculations in relation to location.
  */
+
+
+
 public class Locations {
     private int distance;
+    private static int tot_Distance = 2051; 
+    public int player_Distance = 0;
 
-    /**
+    public final String Independace = "Independace";
+    public final String Courthouse = "Courthouse";
+    public final String Chimney = "Chimney Rock";
+    public final String Laramie = "Fort Laramie";
+    public final String iRock = "Independace Rock";
+    
+
+    public int setPlayerPostion(int travel){
+        player_Distance = player_Distance + travel;
+        return player_Distance;
+    }
+
+    /*public String getPlayerPostion(){
+        if(player_Distance == 0){
+            return "At Independace";
+        }
+        else if(player_Distance<566){
+            return Integer.toString(566-player_Distance)+" From Courthouse Rock";
+        }
+        else if(player_Distance == 566){
+            return "At Courthouse Rock";
+        }
+        else if(player_Distance<(566+21)){
+            return Integer.toString(566+21-player_Distance)+" From Courthouse Rock";
+        }
+        else 
+            return "huh";
+    }*/
+
+    /** 
     *Uses a varible from the main code to manually assign the distance varible a value
     *@param int distance is the value of the range the player is from the final destination
     *@return null
@@ -35,16 +69,18 @@ public class Locations {
     public void setDistance(int distance){
         this.distance = distance;
     }
-
+}
     /**
     *Used to update the distance the player is from the target location
     *@param int travel speed is the amount of distance that can be traveled in one "turn"
     *@return int distance is the updated value that the player is from the final destination
     */
-    public int calculateDistance(int travelSpeed){
+    /*public int calculateDistance(int travelSpeed){
         distance -= travelSpeed;
         if (distance < 0)
             distance = 0;
         return distance;
     }
+
 }
+*/
