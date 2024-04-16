b0e31b0fd9bd9b7e4788bc53075747c85d5bca20
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2024-04-15 20:22:01 -0400

    Update Item.java

--- oregonTrail/Item.java
+++ oregonTrail/Item.java
@@ -1,34 +1,10 @@
-import java.util.ArrayList;
-import java.lang.*;
 
-public class Item {
-    public float String = "apple vinegar";
-    public float String = "bacon";
-    public float String = "beans";
-    public float String = "coffee";
-    public float String = "dried apples";
-    public float String = "flour";
-    public float String = "hardtack";
-    public float String = "lard";
-    public float String = "salt";
-    public float String = "sugar";
-    public float String = "rice";
-    public float String = "water";
-    public float String = "whiskey";
-    public float String = "bedroll";
-    public float String = "blacksmithing tools";
-    public float String = "books";
-    public float String = "medicine";
-    public float String = "cast iron stove";
-    public float String = "chair";
-    public float String = "cookware & eating utensils";
-    public float String = "granny's clock";
-    public float String = "gun making tools";
-    public float String = "keepsakes";
-    public float String = "lead shot";
-    public float String = "mirror";
-    public float String = "gunpowder";
-    public float String = "tent & gear";
-    public float String = "tools";
-    public float String = "Toys";
+public interface Item {
+    public final String FOOD = "food";
+    public final String OXEN = "oxen";
+    public final String WAGON_TONGUE = "tongue";
+    public final String WAGON_WHEEL = "wheel";
+    public final String WAGON_AXLE = "axle";
+    public final String CLOTHES = "clothes";
+    public final String BULLETS = "bullets";
 }
