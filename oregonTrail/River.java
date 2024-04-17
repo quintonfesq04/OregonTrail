ab862004510e764c1fbec05eabdc0f8671deb67a
maddiescott <m-scott.8@onu.edu>
2024-04-16 22:00:53 -0400

    Update River.java

--- oregonTrail/River.java
+++ oregonTrail/River.java
@@ -1,38 +1,70 @@
-import java.util.Random;
 
-import javax.swing.JOptionPane;
+/**
+ * File: River.java
+ * Author: Madison Scott
+ * Date: 4/8/24
+ * Description: This program represents a river using attributes 
+ * like its name, height, flow rate, and width. The crossRiver() method 
+ * simulates crossing attempts with random success chances based on the river's features.
+ */
 
-public class River extends Locations {
-    private String name;
-    private int height;
-    private int flow;
-    private int width;
 
-    public River(String name, int height, int flow, int width) {
-        super(0); // Calls the constructor of the parent class Locations with a distance of 0
-        this.name = name;
-        this.height = height;
-        this.flow = flow;
-        this.width = width;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean crossRiver() {
-        // Random chance of crossing success
-        Random random = new Random();
-        int crossingChance = random.nextInt(10); // Generating a random number between 0 to 9
-
-        int successThreshold = (int) ((100 - height) * (100 - flow) * (100 - width) * 0.25); // Adjusted based on height, flow, and width
-
-        if (crossingChance < successThreshold) {
-            JOptionPane.showMessageDialog(null,"Successfully crossed the river " + name);
-            return true;
-        } else {
-            JOptionPane.showMessageDialog(null,"Failed to cross the river " + name);
-            return false;
-        }
-    }
-}
+ import java.util.Random;
+ import javax.swing.JOptionPane;
+ 
+ public class River extends Locations {
+     // Attributes
+     private String name; // Name of the river
+     private int height; // Height of the river (in meters)
+     private int flow; // Flow rate of the river (in cubic meters per second)
+     private int width; // Width of the river (in meters)
+ 
+     /**
+      * Constructor for River class.
+      *
+      * @param name   The name of the river.
+      * @param height The height of the river (in meters).
+      * @param flow   The flow rate of the river (in cubic meters per second).
+      * @param width  The width of the river (in meters).
+      */
+     public River(String name, int height, int flow, int width) {
+         super(0); // Calls the constructor of the parent class Locations with a distance of 0
+         this.name = name;
+         this.height = height;
+         this.flow = flow;
+         this.width = width;
+     }
+ 
+     /**
+      * Get the name of the river.
+      *
+      * @return The name of the river.
+      */
+     public String getName() {
+         return name;
+     }
+ 
+     /**
+      * Attempt to cross the river.
+      *
+      * @return True if the crossing is successful, false otherwise.
+      */
+     public boolean crossRiver() {
+         // Random chance of crossing success
+         Random random = new Random();
+         int crossingChance = random.nextInt(10); // Generating a random number between 0 to 9
+ 
+         // Calculate success threshold based on height, flow, and width
+         int successThreshold = (int) ((100 - height) * (100 - flow) * (100 - width) * 0.25);
+ 
+         // Check if the crossing is successful based on random chance and success threshold
+         if (crossingChance < successThreshold) {
+             JOptionPane.showMessageDialog(null, "Successfully crossed the river " + name);
+             return true;
+         } else {
+             JOptionPane.showMessageDialog(null, "Failed to cross the river " + name);
+             return false;
+         }
+     }
+ }
+ 
