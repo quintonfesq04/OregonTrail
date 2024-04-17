359daff02508a652a28fb08a8e61fc56d473392b
quintonfesq04 <q-fesq@onu.edu>
2024-04-17 03:57:20 -0400

    Update

--- oregonTrail/StoreScreen.java
+++ oregonTrail/StoreScreen.java
@@ -26,16 +26,6 @@ public class StoreScreen extends JFrame {
         lblNewLabel.setBounds(26, 17, 265, 16);
         panel.add(lblNewLabel);
 
-        JButton btnNewButton = new JButton("No I Do Not Want To Travel To Oregon");
-        btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
-        btnNewButton.setBackground(new Color(251, 252, 255));
-        btnNewButton.setForeground(new Color(0, 0, 0));
-        btnNewButton.setBounds(6, 45, 311, 55);
-        panel.add(btnNewButton);
-
         JButton btnNewButton_1 = new JButton("Buy");
         btnNewButton_1.addActionListener(new ActionListener() {
             @Override
@@ -50,11 +40,65 @@ public class StoreScreen extends JFrame {
         });
         btnNewButton_1.setBackground(new Color(251, 252, 255));
         btnNewButton_1.setForeground(new Color(0, 0, 0));
-        btnNewButton_1.setBounds(329, 45, 117, 55);
+        btnNewButton_1.setBounds(155, 111, 117, 55);
         panel.add(btnNewButton_1);
 
         // Set the content pane of StoreScreen to the panel
         setContentPane(panel);
+        
+        JSpinner spinner = new JSpinner();
+        spinner.setBounds(99, 73, 34, 26);
+        panel.add(spinner);
+        
+        JSpinner spinner_1 = new JSpinner();
+        spinner_1.setBounds(99, 111, 34, 26);
+        panel.add(spinner_1);
+        
+        JSpinner spinner_2 = new JSpinner();
+        spinner_2.setBounds(99, 149, 34, 26);
+        panel.add(spinner_2);
+        
+        JSpinner spinner_3 = new JSpinner();
+        spinner_3.setBounds(99, 187, 34, 26);
+        panel.add(spinner_3);
+        
+        JSpinner spinner_4 = new JSpinner();
+        spinner_4.setBounds(99, 227, 34, 26);
+        panel.add(spinner_4);
+        
+        JLabel lblNewLabel_1 = new JLabel("Food:");
+        lblNewLabel_1.setBounds(26, 78, 61, 16);
+        panel.add(lblNewLabel_1);
+        
+        JLabel lblNewLabel_2 = new JLabel("Water:");
+        lblNewLabel_2.setBounds(26, 116, 61, 16);
+        panel.add(lblNewLabel_2);
+        
+        JLabel lblNewLabel_3 = new JLabel("Oxen:");
+        lblNewLabel_3.setBounds(26, 154, 61, 16);
+        panel.add(lblNewLabel_3);
+        
+        JLabel lblNewLabel_4 = new JLabel("Clothing:");
+        lblNewLabel_4.setBounds(26, 192, 61, 16);
+        panel.add(lblNewLabel_4);
+        
+        JLabel lblNewLabel_5 = new JLabel("Bullets:");
+        lblNewLabel_5.setBounds(26, 232, 61, 16);
+        panel.add(lblNewLabel_5);
+        
+        JLabel lblNewLabel_6 = new JLabel("Total Weight:");
+        lblNewLabel_6.setHorizontalAlignment(SwingConstants.LEFT);
+        lblNewLabel_6.setBounds(145, 78, 92, 16);
+        panel.add(lblNewLabel_6);
+        
+        JLabel lblNewLabel_7 = new JLabel("Items:");
+        lblNewLabel_7.setHorizontalAlignment(SwingConstants.LEFT);
+        lblNewLabel_7.setBounds(26, 45, 117, 16);
+        panel.add(lblNewLabel_7);
+        
+        JLabel lblNewLabel_8 = new JLabel("0");
+        lblNewLabel_8.setBounds(249, 78, 61, 16);
+        panel.add(lblNewLabel_8);
 
         // Set frame properties
        // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
