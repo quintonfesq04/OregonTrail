7285e7aa3e58de79599e4f593d9e13e016db8614
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2024-04-18 14:29:17 -0400

    Create Display.java
    Started Display.java which now uses a CardLayout to display the different screens.

---
+++ oregonTrail/Display.java
@@ -0,0 +1,50 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+
+/**
+ * Display.java generates the JFrame for the Oregon Trail video game
+ * @author Ethan Burch
+ * @version 1.0 4/18/2024
+ */
+public class Display {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Display window = new Display();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Display() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, "name_710703828062700");
+	}
+}
