1ba0f62f65d35008fb52745aa89355bc28d1c967
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2024-04-19 12:57:57 -0400

    Attempt to get keyLsitener

--- oregonTrail/Display.java
+++ oregonTrail/Display.java
@@ -99,7 +99,6 @@ public class Display extends JFrame{
 		this.setVisible(true);
         TravelScreen travelScreen = new TravelScreen(locations);
 		
-<<<<<<< Updated upstream
 		this.getContentPane().add(travelScreen.getPanel(), "TravelScreen");
 		this.getContentPane().add(startScreen.getPanel(), "StartScreen");
 		this.getContentPane().add(storeScreen.getPanel(), "StoreScreen");
@@ -113,22 +112,6 @@ public class Display extends JFrame{
 		this.getContentPane().add(wagonGame.getPanel(), "WagonGame");
 		this.getContentPane().add(deathScreen.getPanel(), "DeathScreen");
 		
-=======
-		this.getContentPane().add(travelScreen.getPanel(), "travelScreen");
-		this.requestFocusInWindow();
-		this.getContentPane().requestFocus();
-		this.addKeyListener(new KeyAdapter(){
-            public void keyPressed(KeyEvent e){
-                int vk = e.getKeyCode();
-                if(vk == KeyEvent.VK_M){
-                    Map map = new Map();
-                    map.display();
-                }
-                else 
-                    System.out.println(vk);
-            }
-        });
->>>>>>> Stashed changes
 		System.out.println(this.getFocusOwner());
 		
 	}
