e7210ac7bbc02d73e4024d5eaf50ed16191c6cf6
quintonfesq04 <q-fesq@onu.edu>
2024-04-19 11:23:05 -0400

    death screen

--- oregonTrail/DeathScreen.java
+++ oregonTrail/DeathScreen.java
@@ -1,18 +1,37 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import java.io.File;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DeathScreen extends AbstractScreen{
     private JPanel panel = new JPanel();
+    private JFrame frame = new JFrame();
 
     @Override
     protected void initialize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+        frame = new JFrame();
+		frame.setBackground(new Color(78, 78, 78));
+		frame.getContentPane().setBackground(new Color(78, 78, 78));
+		frame.setBounds(100, 100, 400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+        File image = new File("Images/death screen.jpg");
+
+        viewPanel = new PicPanel(image);
+        viewPanel.setBackground(Color.BLUE);
+        panel.add(viewPanel);
+        viewPanel.repaint();
     }
 
     @Override
     public void resizeImages() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resizeImages'");
+        viewPanel.resizeImage();
     }
 
     @Override
