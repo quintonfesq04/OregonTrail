b703540b8fdf5268cc184e3c239b96e88af5a880
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2024-04-19 17:18:02 -0400

    Added a cloud and fixed Map issues

--- oregonTrail/TravelScreen.java
+++ oregonTrail/TravelScreen.java
@@ -1,7 +1,10 @@
 import java.awt.event.KeyEvent;
+import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
 import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
@@ -11,32 +14,27 @@ import javax.swing.JPanel;
  * @version 1.0.0 4/18/24
  */
 public class TravelScreen extends AbstractScreen {
-    private JPanel panel = new JPanel();
+    private PicPanel cloud = new PicPanel(new File("Images\\Cloud.png"));
 
     protected PicPanel viewPanel = new PicPanel(new File("Images\\Covered Wagon.jpg"));
-    
+
     private Locations location;
 
     public TravelScreen(Locations location){
         this.location = location;
+        initialize();
     }
 
     protected void initialize(){
         viewPanel.setFocusable(true);
         viewPanel.requestFocusInWindow();
-        //viewPanel.addKeyListener(new MapChecker(location));
-        viewPanel.addKeyListener( new KeyAdapter(){
-            public void keyPressed(KeyEvent e){
-                viewPanel.requestFocusInWindow();
-                int vk = e.getKeyCode();
-                if(vk == KeyEvent.VK_M){
-                    Map map = new Map();
-                    map.display();
-                }
-                else 
-                    System.out.println(vk);
-            }
-        });
+        viewPanel.setLayout(null);
+        cloud.setPreferredSize(new Dimension(100,100));
+        cloud.setSize(cloud.getPreferredSize());
+        cloud.setOpaque(false);
+        viewPanel.add(cloud);
+        cloud.setLocation(50,50);
+        viewPanel.addKeyListener(new MapChecker(location));
     }
 
     public PicPanel getPanel(){
@@ -45,6 +43,7 @@ public class TravelScreen extends AbstractScreen {
     
     public void resizeImages(){
         viewPanel.resizeImage();
+        //cloud.resizeImage();
     }
 
     
