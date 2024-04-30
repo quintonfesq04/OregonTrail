309db9ae2f19a953cf05aac667f35a72d5501ab9
e-burch3 <143116447+e-burch3@users.noreply.github.com>
2024-04-30 09:54:42 -0400

    Update Wagon.java

--- oregonTrail/Gameplay/Wagon.java
+++ oregonTrail/Gameplay/Wagon.java
@@ -1,5 +1,16 @@
 package Gameplay;
 
 public class Wagon {
+    private Inventory inventory = new Inventory();
+    private Locations locations = new Locations(0);
+    private Conditions conditions = new Conditions(inventory);
+    private Player player = new Player();
+    private Trade trade = new Trade();
+    private Store store = new Store();
+
+    private Wagon(){
+
+    }
+
     
 }
