9aab970d2270272d6b970032bae0302c691455b9
quintonfesq04 <q-fesq@onu.edu>
2024-05-09 15:18:43 -0400

    update conditions

--- oregonTrail/Gameplay/Cloud.java
+++ oregonTrail/Gameplay/Cloud.java
@@ -5,73 +5,97 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-import Gameplay.*;
-import Screens.*;
-import Hunting.*;
-
 /**
- * Cloud.java -- creates a cloud to be shows moving across the screen
+ * Cloud.java -- Creates a cloud to be shown moving across the screen.
+ * 
+ * This class represents a cloud object that moves horizontally across the
+ * screen.
+ * It extends the PicPanel class.
+ * 
  * @author Ethan Burch
  * @version 1.2.1 4/21/24
  */
 public class Cloud extends PicPanel {
 
-    private int x;
-    private int y;
-    private File image;
+    private int x; // X coordinate of the cloud
+    private int y; // Y coordinate of the cloud
+    private File image; // File representing the image for the cloud
 
     /**
-     * constructor for cloud object
-     * @param x the initial x coordinate
-     * @param y the initial y coordinate
-     * @param image the image for the cloud
+     * Constructor for a Cloud object.
+     * 
+     * @param x     The initial x coordinate of the cloud.
+     * @param y     The initial y coordinate of the cloud.
+     * @param image The image file for the cloud.
      */
     public Cloud(int x, int y, File image) {
         this.x = x;
         this.y = y;
         this.image = image;
-        init();
+        init(); // Initialize the cloud
     }
 
     /**
-     * inializes the cloud
+     * Initializes the cloud.
      */
     private void init() {
-        this.setImage(image);
-        this.setOpaque(false);
+        this.setImage(image); // Set the image for the cloud
+        this.setOpaque(false); // Make the cloud transparent
     }
 
     /**
-     * obtain the x coordinate of the cloud
-     * @return the x coordinate
+     * Obtain the x coordinate of the cloud.
+     * 
+     * @return The x coordinate of the cloud.
      */
     public int getX() {
         return this.x;
     }
 
     /**
-     * set the x coordinate for the cloud
-     * @param newX the desired x coordinate
+     * Set the x coordinate for the cloud.
+     * 
+     * @param newX The desired x coordinate for the cloud.
      */
     public void setX(int newX) {
         this.x = newX;
     }
 
     /**
-     * get 
+     * Obtain the y coordinate of the cloud.
+     * 
+     * @return The y coordinate of the cloud.
      */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * Set the y coordinate for the cloud.
+     * 
+     * @param newY The desired y coordinate for the cloud.
+     */
     public void setY(int newY) {
         this.y = newY;
     }
 
+    /**
+     * Update the position of the cloud by a given step.
+     * 
+     * @param step The amount by which to update the position of the cloud.
+     */
     public void update(int step) {
         this.x += step;
     }
 
+    /**
+     * Resize the background image to the specified dimensions.
+     * 
+     * @param backgroundImage The background image to resize.
+     * @param newW            The new width of the image.
+     * @param newH            The new height of the image.
+     * @return The resized image.
+     */
     @Override
     protected BufferedImage resize(Image backgroundImage, int newW, int newH) {
         Image tmp = backgroundImage.getScaledInstance(newW, newH, Image.SCALE_FAST);
--- oregonTrail/Gameplay/Conditions.java
+++ oregonTrail/Gameplay/Conditions.java
@@ -2,11 +2,6 @@ package Gameplay;
 
 import java.util.Random;
 
-import Gameplay.*;
-import Screens.*;
-import Hunting.*;
-
-
 /**
  * Conditions.java -- Controls the conditions on the trail
  * 
@@ -18,115 +13,78 @@ import Hunting.*;
 public class Conditions {
     private static Random random = new Random();
     private String eventInfo;
-    private String weather;
-    private double accumulatedRainfall;
-    private String previousWeather;
 
     private Wagon wagon;
 
-    private boolean weatherChange;
-    
+    private boolean wagonGame;
+    private boolean eventOccurred;
+
     public final static int DROUGHT = 0;
     public final static int RAINING = 1;
     public final static int SNOWING = 2;
+
     private int weatherCondition;
-    
+
     /**
-     * Constructs a Conditions object with the given inventory.
+     * Constructs a Conditions object with the given wagon object.
      *
-     * @param inventory the inventory to be associated with this Conditions object
+     * @param wagon the wagon to be associated with this Conditions object
      */
     public Conditions(Wagon wagon) {
         this.wagon = wagon;
         weatherCondition = 0;
     }
 
-    private boolean isEventOccurred() {
-        int eventChance = random.nextInt(100);
-        return eventChance == 0;
+    /**
+     * Notifies if a random event occurred.
+     * 
+     * @return true if a random event occurred, false otherwise.
+     */
+    public boolean isEventOccurred() {
+        return eventOccurred;
     }
 
     /**
-     * Generates weather conditions based on temperature and average rainfall.
+     * Randomly generates weather conditions.
      *
-     * @param temperature the current temperature
-     * @param avgRainfall the average monthly rainfall
+     * @return an integer representing the current weather conditions
      */
-    public void generateWeather(int temperature, double avgRainfall) {
-        if (temperature > 90) {
-            weather = "Very Hot";
-        } else if (temperature >= 70 && temperature <= 90) {
-            weather = "Hot";
-        } else if (temperature >= 50 && temperature <= 70) {
-            weather = "Warm";
-        } else if (temperature >= 30 && temperature <= 50) {
-            weather = "Cool";
-        } else if (temperature >= 10 && temperature <= 30) {
-            weather = "Cold";
-        } else {
-            weather = "Very Cold";
-        }
-
-        double rainProbability = avgRainfall / 100.0;
-        if (random.nextDouble() < rainProbability) {
-            weather += " and Rainy";
-            double todayRainfall = random.nextDouble() * 2;
-            accumulatedRainfall += todayRainfall;
-        } else {
-            if (temperature <= 30) {
-                weather += " and Snowy";
-                double todaySnowfall = random.nextDouble() * 10;
-                accumulatedRainfall += todaySnowfall / 10.0;
-            } else {
-                weather = previousWeather;
-            }
-        }
+    public int generateWeather() {
+        weatherCondition = random.nextInt(3);
+        return weatherCondition;
     }
 
-    private double getAverageMonthlyPrecipitation() {
-        return 50.0; 
+    /**
+     * Gives the current weather conditions. Does not generate these conditions.
+     * 
+     * @return an integer representing the current weather conditions
+     */
+    public int getWeatherCondition() {
+        return weatherCondition;
     }
 
-    private double calculateThunderstormProbability(double avgPrecipitation) {
-        return avgPrecipitation * 0.01; 
-    }
-
-    private boolean isAfterFortHall() {
-        return true; 
-    }
-
-    private boolean isVeryHot() {
-        return true; 
-    }
-
-    private boolean isMountain() {
-        return true; 
-    }
-
-    private boolean isMayToSeptember() {
-        // Implement logic to check if current month is May to September
-        return true;
+    /**
+     * Notifies if the wagon game should be played.
+     * 
+     * @return true if wagon game should be played, false otherwise
+     */
+    public boolean isWagonGame() {
+        return wagonGame;
     }
 
     /**
      * Handles inventory updates based on random events.
-     *
-     * @return the updated inventory after handling events
      */
     public void handleInventory() {
         eventInfo = "";
-        if (isEventOccurred()) {
-            int event = random.nextInt(50);
+        wagonGame = false;
+        eventOccurred = false;
+        int num = random.nextInt(2);
+        if (num == 0) {
+            eventOccurred = true;
+            int event = random.nextInt(18);
             switch (event) {
                 case 0:
-                    // Weather event
-                    String[] weathers = { "Sunny", "Rainy", "Snowy" };
-                    
-                    String weather = weathers[random.nextInt(weathers.length)];
-                    eventInfo = "Random Weather Event: " + weather + "\n";
-                    break;
-
-                case 1:
                     // Thieves event
                     eventInfo = "Random Event: Thieves attacked your wagon!\n";
                     int stolenAmount = random.nextInt(wagon.getInventory().getFood()) + 1;
@@ -134,205 +92,143 @@ public class Conditions {
                     eventInfo += " " + stolenAmount + " food stolen by thieves.\n";
                     break;
 
-                case 2:
+                case 1:
                     // Wagon breakdown event
                     eventInfo = "Random Event: Your wagon broke down.\n";
-                    String[] wagonParts = {Inventory.WAGON_WHEEL, Inventory.WAGON_TONGUE, Inventory.WAGON_AXLE};
+                    String[] wagonParts = { Inventory.WAGON_WHEEL, Inventory.WAGON_TONGUE, Inventory.WAGON_AXLE };
                     String part = wagonParts[random.nextInt(wagonParts.length)];
                     if (wagon.getInventory().getItemCount(part) > 0) {
                         wagon.getInventory().removeItem(part, 1);
                     } else {
                         eventInfo += " You were unable to fix it!\n";
                     }
+                    wagonGame = true;
                     break;
 
-                case 3:
+                case 2:
                     // Disease event
                     String[] diseases = { "Exhaustion", "Typhoid", "Cholera", "Measles", "Dysentery", "Fever" };
                     String disease = diseases[random.nextInt(diseases.length)];
                     eventInfo = "Random Disease Event: " + disease + "\n";
-                    String partyMember = wagon.getPlayer().getPartyNames()[random.nextInt(wagon.getPlayer().getPartyNames().length)];
-                    eventInfo += partyMember + "is sick with " + disease;
+                    String partyMember = wagon.getPlayer().getPartyNames()[random
+                            .nextInt(wagon.getPlayer().getPartyNames().length)];
+                    eventInfo += partyMember + " is sick with " + disease;
                     break;
 
-                case 4:
+                case 3:
                     // Indians help find food
                     wagon.getInventory().setFood(wagon.getInventory().getFood() + 30);
                     eventInfo = "Indians helped you find food! +30 lbs of food.\n";
                     break;
 
-                case 5:
+                case 4:
                     // Severe thunderstorm
-                    double avgPrecipitation = getAverageMonthlyPrecipitation();
-                    double probability = calculateThunderstormProbability(avgPrecipitation);
-                    if (random.nextDouble() < probability) {
-                        eventInfo = "Random Event: Severe thunderstorm!\n";
-                        int lostFood = random.nextInt(11); // Random amount lost (0-10)
-                        wagon.getInventory().removeItem(Inventory.FOOD, lostFood);
-                        eventInfo += "Lost " + lostFood + " lbs of food due to the severe thunderstorm.\n";
-                    }
+                    eventInfo = "Random Event: Severe thunderstorm!\n";
+                    int lostFood = random.nextInt(11); // Random amount lost (0-10)
+                    wagon.getInventory().removeItem(Inventory.FOOD, lostFood);
+                    eventInfo += "Lost " + lostFood + " lbs of food due to the severe thunderstorm.\n";
+                    break;
+
+                case 5:
+                    // Severe blizzard
+                    eventInfo = "Random Event: Severe blizzard!\n";
+                    // Implement logic for consequences of severe blizzard
+                    lostFood = random.nextInt(11); // Random amount lost (0-10)
+                    wagon.getInventory().removeItem(Inventory.FOOD, lostFood);
+                    eventInfo += "Lost " + lostFood + " lbs of food due to the severe blizzard.\n";
                     break;
 
                 case 6:
-                    // Severe blizzard
-                    if (random.nextDouble() < 0.15) {
-                        eventInfo = "Random Event: Severe blizzard!\n";
-                        // Implement logic for consequences of severe blizzard
-                        int lostFood = random.nextInt(11); // Random amount lost (0-10)
-                        wagon.getInventory().removeItem(Inventory.FOOD, lostFood);
-                        eventInfo += "Lost " + lostFood + " lbs of food due to the severe blizzard.\n";
+                    // Heavy fog
+                    eventInfo = "Random Event: Heavy fog!\n";
+                    if (random.nextDouble() < 0.5) {
+                        eventInfo += "Lost a day's travel due to heavy fog.\n";
                     }
+
                     break;
 
                 case 7:
-                    // Heavy fog
-                    if (isAfterFortHall() && !isVeryHot()) {
-                        if (random.nextDouble() < 0.06) {
-                            eventInfo = "Random Event: Heavy fog!\n";
-                            if (random.nextDouble() < 0.5) {
-                                eventInfo += "Lost a day's travel due to heavy fog.\n";
-                            }
-                        }
-                    }
+                    // Hail storm
+                    eventInfo = "Random Event: Hail storm!\n";
+                    lostFood = random.nextInt(6); // Random amount lost (0-5)
+                    wagon.getInventory().removeItem(Inventory.FOOD, lostFood);
+                    eventInfo += "Lost " + lostFood + " units of medicine due to the hail storm.\n";
                     break;
 
                 case 8:
-                    // Hail storm
-                    if (!isAfterFortHall() && isVeryHot()) {
-                        if (random.nextDouble() < 0.06) {
-                            eventInfo = "Random Event: Hail storm!\n";
-                            int lostFood = random.nextInt(6); // Random amount lost (0-5)
-                            wagon.getInventory().removeItem(Inventory.FOOD, lostFood);
-                            eventInfo += "Lost " + lostFood + " units of medicine due to the hail storm.\n";
-                        }
-                    }
-                    break;
-
-                case 9:
-                    // Injured or dead ox
-                    double oxInjuryProbability = isMountain() ? 0.035 : 0.02;
-                    if (wagon.getInventory().getOxenHealthy() && random.nextDouble() < oxInjuryProbability) {
+                    if (wagon.getInventory().getOxenHealthy()) {
                         eventInfo = "Random Event: An ox has been injured!\n";
                         wagon.getInventory().setOxenHealthy(false);
-                    } else if (!wagon.getInventory().getOxenHealthy() && random.nextDouble() < 0.02) {
+                    } else if (!wagon.getInventory().getOxenHealthy()) {
                         eventInfo = "Random Event: An ox has died!\n";
                         wagon.getInventory().setOxen(wagon.getInventory().getOxen() - 1);
                         wagon.getInventory().setOxenHealthy(true);
                     }
                     break;
 
-                case 10:
+                case 9:
                     // Injured party member (broken arm or leg)
-                    double injuryProbability = isMountain() ? 0.035 : 0.02;
-                    if (random.nextDouble() < injuryProbability) {
-                        eventInfo = "Random Event: A party member is injured!\n";
-                        wagon.getPlayer().getSick();
-                    }
+                    eventInfo = "Random Event: A party member is injured!\n";
+                    wagon.getPlayer().getSick();
+                    break;
+
+                case 10:
+                    // Snake bite
+                    eventInfo = "Random Event: Snake bite! A party member is sick!\n";
+                    wagon.getPlayer().getSick();
+
                     break;
 
                 case 11:
-                    // Snake bite
-                    if (random.nextDouble() < 0.007) {
-                        eventInfo = "Random Event: Snake bite!\n";
-                    }
+                    // Lose trail
+                    eventInfo = "Random Event: Lost the trail! Lost 2 days.\n";
+                    wagon.getPlayer().incrementDaysTraveled(2);
+
                     break;
 
                 case 12:
-                    // Lose trail
-                    if (random.nextDouble() < 0.02) {
-                        eventInfo = "Random Event: Lost the trail!\n";
-                    }
+                    // Wrong trail
+                    eventInfo = "Random Event: Took the wrong trail! Lost 3 days\n";
+                    wagon.getPlayer().incrementDaysTraveled(3);
                     break;
 
                 case 13:
-                    // Wrong trail
-                    if (random.nextDouble() < 0.01) {
-                        eventInfo = "Random Event: Took the wrong trail!\n";
-                    }
+                    // Rough trail
+                    eventInfo = "Random Event: Rough trail! Lost 1 day.\n";
+                    wagon.getPlayer().incrementDaysTraveled();
+
                     break;
 
                 case 14:
-                    // Rough trail
-                    if (isMountain() && random.nextDouble() < 0.025) {
-                        eventInfo = "Random Event: Rough trail!\n";
-                    }
+                    // Impassible trail
+                    eventInfo = "Random Event: Impassible trail! Lost 2 days.\n";
+                    wagon.getPlayer().incrementDaysTraveled(2);
+
                     break;
 
                 case 15:
-                    // Impassible trail
-                    if (isMountain() && random.nextDouble() < 0.025) {
-                        eventInfo = "Random Event: Impassible trail!\n";
-                    }
-                    break;
-
-                case 16:
                     // Finding wild fruit
-                    if (isMayToSeptember() && random.nextDouble() < 0.04) {
-                        eventInfo = "Random Event: Found wild fruit! +20 lbs of food.\n";
-                        wagon.getInventory().setFood(wagon.getInventory().getFood() + 20);
-                    }
+                    eventInfo = "Random Event: Found wild fruit! +20 lbs of food.\n";
+                    wagon.getInventory().setFood(wagon.getInventory().getFood() + 20);
                     break;
 
                 case 17:
-                    // Fire in the wagon
-                    if (random.nextDouble() < 0.02) {
-                        eventInfo = "Random Event: Fire in the wagon!\n";
-                    }
+                    // Lost party member
+                    int daysLost = random.nextInt(6) + 1; // Lose up to 6 days
+                    wagon.getPlayer().incrementDaysTraveled(daysLost);
+                    eventInfo = "Random Event: Lost a party member! Lost " + daysLost + " days.\n";
                     break;
 
                 case 18:
-                    // Lost party member
-                    if (random.nextDouble() < 0.01) {
-                        int daysLost = random.nextInt(6) + 1; // Lose up to 5 days
-                        eventInfo = "Random Event: Lost a party member! Lost " + daysLost + " days.\n";
-                    }
-                    break;
-
-                case 19:
                     // Ox wanders off
-                    if (random.nextDouble() < 0.01) {
-                        int daysLost = random.nextInt(4) + 1; // Lose up to 3 days
-                        eventInfo = "Random Event: Ox wandered off! Lost " + daysLost + " days.\n";
-                    }
-                    break;
-
-                case 20:
-                    // Finding an abandoned wagon
-                    if (random.nextDouble() < 0.02) {
-                        eventInfo = "Random Event: Found an abandoned wagon!\n";
-                    }
-                    break;
-
-                case 21:
-                    // Thief comes during the night
-                    if (random.nextDouble() < 0.02) {
-                        eventInfo = "Random Event: Thief came during the night!\n";
-                    }
-                    break;
-
-                case 22:
-                    // Bad water
-                    if (random.nextDouble() < 0.1 && accumulatedRainfall < 0.1) {
-                        eventInfo = "Random Event: Bad water! Insufficient rainfall.\n";
-                    }
-                    break;
-
-                case 23:
-                    // Very little water
-                    if (random.nextDouble() < 0.2 && accumulatedRainfall < 0.1) {
-                        eventInfo = "Random Event: Very little water! Insufficient rainfall.\n";
-                    }
-                    break;
-
-                case 24:
-                    // Inadequate grass
-                    if (random.nextDouble() < 0.2 && accumulatedRainfall < 0.1) {
-                        eventInfo = "Random Event: Inadequate grass! Insufficient rainfall.\n";
-                    }
+                    daysLost = random.nextInt(4) + 1; // Lose up to 4 days.
+                    wagon.getPlayer().incrementDaysTraveled(daysLost);
+                    eventInfo = "Random Event: Ox wandered off! Lost " + daysLost + " days.\n";
                     break;
 
                 default:
                     // No event occurred
+                    eventInfo = "No event occurred";
                     break;
             }
         }
